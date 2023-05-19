47809afd018cd1a33ce29fc307fa9353673c73ed
PseudoKiwi <tincho.perciante@gmail.com>
2023-05-19 16:30:10 -0300

    polyfit de resultados en main

--- main.py
+++ main.py
@@ -3,9 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from matplotlib.pyplot import xlim, ylim, figure, ion, show
-from numpy import zeros
-from auxiliarFunctions import interactionEnergy, potential
+from matplotlib.pyplot import figure, show
+from numpy import zeros, polyfit, linspace
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -33,10 +32,22 @@ if __name__ == '__main__':
     #    e[:] = a.copy()[:30]
     #    p[:] = b.copy()[:30]
 
+    particulas = []
+    fuerza = []
+    with open("force-loss.txt", "r") as file:
+        for linea in file:
+            aux = linea.split(', ')
+            particulas.append(float(aux[1]))
+            fuerza.append(float(aux[0]))
+    m, b = polyfit(particulas, fuerza, 1)
+    p2 = linspace(min(particulas), max(particulas), 2)
+    f2 = b + m*p2
+
 
     fig3 = figure()
     ax3 = fig3.add_subplot(111)
-    system, = ax3.plot([2,4,4,4,6,7,6,6,6,16,14,19],[8.196310363416938,12.42323983598615,13.150891507499873,13.837511387561603,19.735858338557033,21.333120170063086,23.09459734070445,32.17306003810418,68.3456504108901,64.69017723791073,61.013339448827146,66.2441167672423])
+    system, = ax3.plot(particulas, fuerza, ".")
+    system, = ax3.plot(p2, f2)
 
     #fig3 = figure()
     #ax3 = fig3.add_subplot(111)
